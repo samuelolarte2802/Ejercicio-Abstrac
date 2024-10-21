139ab0713f1be09e2136c4880cf703c90f219c02
Your Name <you@example.com>
2024-10-21 11:29:02 -0500

    Ejercicio

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        //Por Samuel Olarte
         Scanner scan = new Scanner(System.in);
         double altura, base;
         int opc = 0;
